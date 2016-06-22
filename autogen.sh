fb3bf64a98524b1a99e4763ee6ad9eeaabf1462a
Dmitry Boulytchev <dboulytchev@gmail.com>
2016-06-22 17:30:12 +0300

    Excluded duplicated modules

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 set -x
 pushd `dirname $0`
-aclocal
-autoconf --force
-automake --add-missing --copy --foreign
-svn info|grep '^Revision:'|awk '{print "$Revision:",$2,"$"}' > VERSION
+autoreconf -fi
 popd
